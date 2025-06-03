ff646ab4169cef18d50aaeca6b12a44482012d2c
Talles Souza <tallessouza117@gmail.com>
2025-06-03 01:09:54 +0000

    primeira aula

--- A1_0Teste.java
+++ A1_0Teste.java
@@ -1,10 +1,11 @@
 public class A1_0Teste {
     public static void main(String[] args) {
         // Declaração de variáveis inteiras e reais
-        int num1 = 10;      // Tipo inteiro (int)
-        double num2 = 10.5;      // Tipo real 
+        int numeroInteiro = 10;      // Tipo inteiro (int)
+        double numeroReal = 5.75;    // Tipo real (double)
+        
         // Exibindo os valores das variáveis
-        System.out.println("Esse é o num1 " + num1);
-        System.out.println("Esse é o num2 " + num2);
+        System.out.println("Número inteiro: " + numeroInteiro);
+        System.out.println("Número real: " + numeroReal);
     }
 }
